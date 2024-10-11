8330732cfddc4cd35fd39fbd75c531655315488f
jhbafaws <jhbafaws@gmail.com>
2024-10-11 00:40:29 -0300

    basic struture

---
+++ transaction-producer/src/main/java/com/finytec/transaction/bank/transaction_producer/TransactionProducerApplication.java
@@ -0,0 +1,13 @@
+package com.finytec.transaction.bank.transaction_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionProducerApplication.class, args);
+	}
+
+}
